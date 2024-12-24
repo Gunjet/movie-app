83287fecc18951b2c2ede47556130de0832e2aca
24LP8321 24LP8321 <24LP8321@G302-CC0181.local>
2024-12-24 18:11:22 +0800

    added api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,69 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+      {/* <footer className="w-full h-[280px] bg-[#4338CA] flex justify-center items-center absolute bottom-0 left-0">
+        <div className="w-[1280px] h-[200px] flex justify-between">
+          <div className="w-[247px] h-[200px] text-white flex flex-col gap-3">
+            <div className="flex gap-2">
+             <img src='/film.png' className='w-[20px] h-[20px]'/>
+             <p className='text-[16px] italic'>Movie Z</p>
+            </div>
+            <p className='text-[14px]'>© 2024 Movie Z. All Rights Reserved.</p>
+          </div>
+          <div className="w-[913px] h-[200px] flex justify-between">
+            <div className='w-[274px] h-[52px]'></div>
+            <div className='w-[174px] h-[200px] text-white flex flex-col gap-3 text-[14px]'>
+              <p>Contact Information</p>
+              <div className="flex gap-3 items-center">
+                <img src='/email.png' className='w-[16px] h-[16px]'/>
+                <p>Email: support@movieZ.com</p>
+              </div>
+              <div className="flex gap-3 items-center">
+                <img src='/phone.png' className='w-[16px] h-[16px]'/>
+                <p>Phone: <br/> +976 (11) 123-4567</p>
+              </div>
+            </div>
+            <div className='w-[274px] h-[52px] text-white flex flex-col gap-3 text-[14px]'>
+              <p>Follow us</p>
+              <p>Facebook Instagram Twitter Youtube</p>
+            </div>
+          </div>
+        </div>
+      </footer> */}
+
+      <footer className="w-full h-[308px] bg-[#4338CA] absolute bottom-0 left-0 flex justify-center items-center">
+        <div className='w-[335px] h-[228px] flex flex-col justify-between'>
+          <div className="w-[247px] h-[52px] text-white flex flex-col gap-3 ">
+            <div className="flex gap-2">
+             <img src='/film.png' className='w-[20px] h-[20px]'/>
+             <p className='text-[16px] italic'>Movie Z</p>
+            </div>
+            <p className='text-[14px]'>© 2024 Movie Z. All Rights Reserved.</p>
+          </div>
+          <div className='flex justify-between w-[335px] h-[148px]'>
+            <div className='h-[136px] w-[174px] text-[14px] text-white flex flex-col justify-between'>
+            <p>Contact Information</p>
+              <div className="flex gap-3 items-center">
+                <img src='/email.png' className='w-[16px] h-[16px]'/>
+                <p>Email: support@movieZ.com</p>
+              </div>
+              <div className="flex gap-3 items-center">
+                <img src='/phone.png' className='w-[16px] h-[16px]'/>
+                <p>Phone: <br/> +976 (11) 123-4567</p>
+              </div>
+            </div>
+            <div className='h-[148px] w-[113px] flex flex-col justify-between text-white text-[14px]'>
+              <p>Follow us</p>
+              <p className='font-semibold'>Facebook</p>
+              <p className='font-semibold'>Instagram</p>
+              <p className='font-semibold'>Twitter</p>
+              <p className='font-semibold'>Youtube</p>
+            </div>
+          </div>
+        </div>
+      </footer>
       </body>
+     
     </html>
   );
 }
